9ef8229ae3c5c2cd2c17169d6c21aea2ded0c29d
ronfe <chihungfei@126.com>
2015-05-20 19:49:46 +0800

    Add station name apaptation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
 var express = require('express');
 var request = require('request');
+var station = require(__dirname + '/chinarail/station');
 var fs = require('fs');
 var _ = require('lodash');
 var path = require('path');
@@ -25,47 +26,51 @@ app.get('/info/:date/:from/:to', function(req, res){
     var date = req.params.date;
     var fromS = req.params.from;
     var toS = req.params.to;
+    var fromSCode = station.getStationCode(fromS);
+    var toSCode = station.getStationCode(toS);
     var options = {
-        url: 'http://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=ADULT&queryDate=' + date + '&from_station=' + fromS + '&to_station=' + toS
+        url: 'http://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=ADULT&queryDate=' + date + '&from_station=' + fromSCode + '&to_station=' + toSCode
     };
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var newTrainInfo = [];
-            var jsonTrainInfo = json.parse(body);
-            _.each(jsonTrainInfo.data.datas, function(item){
-                var filterTrainInfo = _.pick(item, [
-                    'station_train_code',
-                    'start_station_telecode',
-                    'start_station_name',
-                    'end_station_telecode',
-                    'end_station_name',
-                    'from_station_telecode',
-                    'from_station_name',
-                    'to_station_telecode',
-                    'to_station_name',
-                    'start_time',
-                    'arrive_time',
-                    'lishi',
-                    'sale_time',
-                    'gg_num',
-                    'gr_num',
-                    'qt_num',
-                    'rw_num',
-                    'rz_num',
-                    'tz_num',
-                    'wz_num',
-                    'yb_num',
-                    'yw_num',
-                    'yz_num',
-                    'ze_num',
-                    'zy_num',
-                    'swz_num'
-                ]);
-                newTrainInfo.push(filterTrainInfo);
-            });
-            res.status(200).send(newTrainInfo);
-        }
-    });
+    console.log(options.url);
+    res.status(200).send(options.url);
+    //request(options, function (error, response, body) {
+    //    if (!error && response.statusCode == 200) {
+    //        var newTrainInfo = [];
+    //        var jsonTrainInfo = json.parse(body);
+    //        _.each(jsonTrainInfo.data.datas, function(item){
+    //            var filterTrainInfo = _.pick(item, [
+    //                'station_train_code',
+    //                'start_station_telecode',
+    //                'start_station_name',
+    //                'end_station_telecode',
+    //                'end_station_name',
+    //                'from_station_telecode',
+    //                'from_station_name',
+    //                'to_station_telecode',
+    //                'to_station_name',
+    //                'start_time',
+    //                'arrive_time',
+    //                'lishi',
+    //                'sale_time',
+    //                'gg_num',
+    //                'gr_num',
+    //                'qt_num',
+    //                'rw_num',
+    //                'rz_num',
+    //                'tz_num',
+    //                'wz_num',
+    //                'yb_num',
+    //                'yw_num',
+    //                'yz_num',
+    //                'ze_num',
+    //                'zy_num',
+    //                'swz_num'
+    //            ]);
+    //            newTrainInfo.push(filterTrainInfo);
+    //        });
+    //        res.status(200).send(newTrainInfo);
+    //    }
+    //});
 });
 
 app.listen(2413);
